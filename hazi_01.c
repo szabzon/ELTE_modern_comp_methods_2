4498b1a449ec0f20ca866d240ee00c8d99bfc45d
szabzon <79698718+szabzon@users.noreply.github.com>
2021-09-11 11:35:32 +0200

    Update hazi_01.c

--- hazi_01.c
+++ hazi_01.c
@@ -1,3 +1,4 @@
+//Ez az amit ki tudtam hozni, sajnos nincs semmi áttörés, csak tömören írtam le a kódot. Interneten is kerestem, nem találtam nagyon rövidebb algoritmust, pedig biztos van.
 #include <stdio.h>
 #include <stdlib.h>
 int main(){int x=6,i;for(i=x-1;i>1;i--){x=x*i;}if(x==0){x=1;}printf("%d",x);return 0;}
